76680ddae9234df6cab36745f032d707d255de3d
Bryan-Nelson-2 <bryan.nelson@stu.ocsb.ca>
2025-10-02 13:30:24 -0400

    Changed comments

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Alex
+ * Created on: Sep 2025
+ * This program shows the colours red, blue, green, red and blue, red and green, blue and green and red blue and green in order.
 */
 
 basic.showString('Hello, World!')
